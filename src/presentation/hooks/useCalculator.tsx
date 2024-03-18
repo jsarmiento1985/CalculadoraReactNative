234a9245312284a297a0a6a4aa752ce786c2ba01
Furia55 <jsarmiento1985@hotmail.com>
2024-03-18 16:01:21 -0500

    se termina calculadora, completamente funcional

--- src/presentation/hooks/useCalculator.tsx
+++ src/presentation/hooks/useCalculator.tsx
@@ -1,132 +1,193 @@
-import React, { useRef, useState } from 'react'
-enum Operator{
-    add,
-    subtract,
-    multiply,
-    divide,
+import React, { useEffect, useRef, useState } from 'react'
+enum Operator {
+    add = '+',
+    subtract = '-',
+    multiply = 'x',
+    divide = '÷',
 }
 
 export const useCalculator = () => {
-  
-  const [number, setNumber] = useState('0');
-  const [prevNumber, setPrevNumber] = useState('0');
 
-  const lastOperation = useRef<Operator>();
-  
-  const clean = () => {
-    setNumber('0' );
-    setPrevNumber('0');
-  }
-   // Borrar el ultimo numero digitado
-  const deleteOperation = () =>{
+    const [formula, setFormula] = useState('');
+    const [number, setNumber] = useState('0');
+    const [prevNumber, setPrevNumber] = useState('0');
 
-    let currentSign = '';
-    let temporalNumber = number;
+    const lastOperation = useRef<Operator>();
 
-    if(number.includes('-')){
-        currentSign = '-';
-        temporalNumber = number.substring(1);// quita el simbolo negativo del numero  
-    }
-
-    if(temporalNumber.length > 1 ){
-        return setNumber(currentSign + temporalNumber.slice(0,-1));
-    }   
-
-    setNumber('0');
-
-  } 
-
-  const toggleSign = () =>{
-    if(number.includes('-')){
-        return setNumber(number.replace('-',''))
-    }
-    setNumber('-' + number);
-  }
-
-  const buildNumber = ( numberString: string ) => {
-
-    // no mas de un punto .
-    if(number.includes('.') && numberString === '.') return;
-
-    // si inica con cero y no tiene punto decimal entonces concatena y muestra
-    if(number.startsWith('0') || number.startsWith('-0')){
-        
-        
-        if(numberString === '.'){
-            return setNumber(number + numberString);
+    useEffect(() => {
+        if (lastOperation.current) {// si hay un simbolo
+            const firstFormulaPart = formula.split(' ').at(0);// primera posiicion
+            setFormula(`${firstFormulaPart} ${lastOperation.current} ${number}`);
+        } else {
+            setFormula(number);
         }
+
+
+
+    }, [number])
+
+
+    useEffect(() => {
+        const subResult = calculateSubResult();
+        setPrevNumber (`${ subResult }`);
+    }, [formula])
     
 
-        //evaluar otro cero y no hay punto
-        if(numberString === '0' && number.includes('.')){
+    const clean = () => {
+        setNumber('0');
+        setPrevNumber('0');
+        lastOperation.current = undefined;
+        setFormula('');
+    }
+    // Borrar el ultimo numero digitado
+    const deleteOperation = () => {
+
+        let currentSign = '';
+        let temporalNumber = number;
+
+        if (number.includes('-')) {
+            currentSign = '-';
+            temporalNumber = number.substring(1);// quita el simbolo negativo del numero  
+        }
+
+        if (temporalNumber.length > 1) {
+            return setNumber(currentSign + temporalNumber.slice(0, -1));
+        }
+
+        setNumber('0');
+
+    }
+
+    const toggleSign = () => {
+        if (number.includes('-')) {
+            return setNumber(number.replace('-', ''))
+        }
+        setNumber('-' + number);
+    }
+
+    const buildNumber = (numberString: string) => {
+
+        // no mas de un punto .
+        if (number.includes('.') && numberString === '.') return;
+
+        // si inica con cero y no tiene punto decimal entonces concatena y muestra
+        if (number.startsWith('0') || number.startsWith('-0')) {
+
+
+            if (numberString === '.') {
+                return setNumber(number + numberString);
+            }
+
+
+            //evaluar otro cero y no hay punto
+            if (numberString === '0' && number.includes('.')) {
+                return setNumber(number + numberString);
+            }
+
+            // evaluar si es diferente de cero y no hay punto decimal y es el primer numero
+            if (numberString !== '0' && !number.includes('.')) {
+                return setNumber(numberString);
+            }
+
+            //evaluar evitar el 000000.00 al menos que haya un punto decimal(.)
+            if (numberString === '0' && !number.includes('.')) {
+                return;
+            }
+
+
             return setNumber(number + numberString);
         }
+        setNumber(number + numberString);
 
-        // evaluar si es diferente de cero y no hay punto decimal y es el primer numero
-        if(numberString !=='0' && !number.includes('.') ){
-            return setNumber(numberString);
-        }
-
-        //evaluar evitar el 000000.00 al menos que haya un punto decimal(.)
-        if(numberString ==='0' && !number.includes('.')){
-            return;
-        }
-
-
-    return setNumber( number  + numberString );
-  }
-  setNumber( number  + numberString );
-
-}
-
-const setLastNumber = () =>{
-
-    if( number.endsWith('.') ){
-        setPrevNumber(number.slice(0, -1));
-    }else{
-        setPrevNumber(number);
     }
-    setNumber('0');
-}
+
+    const setLastNumber = () => {
+        calculateResult();
+        if (number.endsWith('.')) {
+            setPrevNumber(number.slice(0, -1));
+        } else {
+            setPrevNumber(number);
+        }
+        setNumber('0');
+    }
 
 
-const divideOperation = () =>{
-    setLastNumber();
-    lastOperation.current = Operator.divide;
-}
+    const divideOperation = () => {
+        setLastNumber();
+        lastOperation.current = Operator.divide;
+    }
 
-const multiplyOperation = () =>{
-    setLastNumber();
-    lastOperation.current = Operator.multiply;
-}
+    const multiplyOperation = () => {
+        setLastNumber();
+        lastOperation.current = Operator.multiply;
+    }
 
-const subtractOperation = () =>{
-    setLastNumber();
-    lastOperation.current = Operator.subtract;
-}
+    const subtractOperation = () => {
+        setLastNumber();
+        lastOperation.current = Operator.subtract;
+    }
 
-const addOperation = () =>{
-    setLastNumber();
-    lastOperation.current = Operator.add;
-}
+    const addOperation = () => {
+        setLastNumber();
+        lastOperation.current = Operator.add;
+    }
 
-    
 
- return {
-     //properties
-     number,
-     prevNumber,
-    //methods
-     buildNumber,
-     toggleSign,
-     clean,
-     deleteOperation,
-     divideOperation,
-     multiplyOperation,
-     subtractOperation,
-     addOperation,
+    const calculateResult = () => {
+       const result =  calculateSubResult();
+       setFormula( `${result}`);
+       lastOperation.current = undefined;
+       setPrevNumber('0');
+    }
 
-  }
-   
-  
+    const calculateSubResult = (): number => {
+        const [firstValue, operation, secondValue] = formula.split(' ');
+
+        const num1 = Number(firstValue);//NaN
+        const num2 = Number(secondValue);//NaN
+
+        if (isNaN( num2 ))  return num1; // si num2 NO es un numero
+        
+
+        switch ( operation ) {
+
+            case Operator.add:
+                return num1 + num2;
+          
+            case Operator.subtract:
+                return num1 - num2;
+               
+            case Operator.multiply:
+                return num1 * num2;
+             
+            case Operator.divide:
+               return num1 / num2;
+               
+            default:
+                throw new Error("Operation not implemented");
+
+        }
+
+    }
+
+    return {
+        //properties
+        number,
+        prevNumber,
+        formula,
+        //methods
+        buildNumber,
+        toggleSign,
+        clean,
+        deleteOperation,
+        divideOperation,
+        multiplyOperation,
+        subtractOperation,
+        addOperation,
+        calculateResult,
+        calculateSubResult,
+
+    }
+
+
 }
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -5,63 +5,70 @@ import { CalculatorButton } from '../components/CalculatorButton'
 import { useCalculator } from '../hooks/useCalculator'
 
 export const CalculatorScreen = () => {
-    
+
     const {
-        number, buildNumber, toggleSign, prevNumber, clean, deleteOperation, divideOperation, multiplyOperation, subtractOperation, addOperation,
+        formula, number, buildNumber, toggleSign, prevNumber, clean, deleteOperation, divideOperation, multiplyOperation, subtractOperation, addOperation, calculateResult,
     } = useCalculator();
-    
-  
+
 
     return (
         <View style={styles.calculatorContainer}>
 
             <View style={{ paddingHorizontal: 30, paddingBottom: 20 }}>
-                <Text 
+                <Text
                     adjustsFontSizeToFit
                     numberOfLines={1}
-                    style={styles.mainResult}>{number}
+                    style={styles.mainResult}>{formula}
                 </Text>
 
-                <Text 
-                    adjustsFontSizeToFit
-                    numberOfLines={1}
-                    style={styles.subResult}>
-                        {( prevNumber) === '0' ? ' ': prevNumber }
-                </Text>
+
+                {
+                    (formula === prevNumber)
+                        ? <Text style={styles.subResult}></Text>
+                        : (
+                            <Text
+                                adjustsFontSizeToFit
+                                numberOfLines={1}
+                                style={styles.subResult}>
+                                { prevNumber }
+                            </Text>
+                        )
+                }
+
             </View>
 
             <View style={styles.row}>
-                <CalculatorButton onPress={ clean } label = "C" color ={colors.lightGray} blackText={true} />
-                <CalculatorButton onPress={ toggleSign } label = "+/-" color ={colors.lightGray} blackText={true}/>
-                <CalculatorButton onPress={ deleteOperation } label = "del" color ={colors.lightGray} blackText={true} />
-                <CalculatorButton onPress={ divideOperation } label = "÷" color ={colors.orange} />
+                <CalculatorButton onPress={clean} label="C" color={colors.lightGray} blackText={true} />
+                <CalculatorButton onPress={toggleSign} label="+/-" color={colors.lightGray} blackText={true} />
+                <CalculatorButton onPress={deleteOperation} label="del" color={colors.lightGray} blackText={true} />
+                <CalculatorButton onPress={divideOperation} label="÷" color={colors.orange} />
             </View>
 
             <View style={styles.row}>
-                <CalculatorButton onPress={()=> buildNumber("7")} label = "7"/>
-                <CalculatorButton onPress={()=> buildNumber("8")} label = "8"/>
-                <CalculatorButton onPress={()=> buildNumber("9")} label = "9"/>
-                <CalculatorButton onPress={ multiplyOperation } label = "x" color ={colors.orange}/>
+                <CalculatorButton onPress={() => buildNumber("7")} label="7" />
+                <CalculatorButton onPress={() => buildNumber("8")} label="8" />
+                <CalculatorButton onPress={() => buildNumber("9")} label="9" />
+                <CalculatorButton onPress={multiplyOperation} label="x" color={colors.orange} />
             </View>
 
             <View style={styles.row}>
-                <CalculatorButton onPress={()=> buildNumber("4")} label = "4"/>
-                <CalculatorButton onPress={()=> buildNumber("5")} label = "5"/>
-                <CalculatorButton onPress={()=> buildNumber("6")} label = "6"/>
-                <CalculatorButton onPress={ subtractOperation } label = "-" color ={colors.orange}/>
+                <CalculatorButton onPress={() => buildNumber("4")} label="4" />
+                <CalculatorButton onPress={() => buildNumber("5")} label="5" />
+                <CalculatorButton onPress={() => buildNumber("6")} label="6" />
+                <CalculatorButton onPress={subtractOperation} label="-" color={colors.orange} />
             </View>
 
             <View style={styles.row}>
-                <CalculatorButton onPress={()=> buildNumber("1")} label = "1"/>
-                <CalculatorButton onPress={()=> buildNumber("2")} label = "2"/>
-                <CalculatorButton onPress={()=> buildNumber("3")} label = "3"/>
-                <CalculatorButton onPress={ addOperation } label = "+" color ={colors.orange}/>
+                <CalculatorButton onPress={() => buildNumber("1")} label="1" />
+                <CalculatorButton onPress={() => buildNumber("2")} label="2" />
+                <CalculatorButton onPress={() => buildNumber("3")} label="3" />
+                <CalculatorButton onPress={addOperation} label="+" color={colors.orange} />
             </View>
 
             <View style={styles.row}>
-                <CalculatorButton onPress={()=> buildNumber("0")} label = "0" doubleSize = { true }/>
-                <CalculatorButton onPress={()=> buildNumber(".")} label = "."/>
-                <CalculatorButton onPress={()=> console.log("=")} label = "=" color ={colors.orange}/>
+                <CalculatorButton onPress={() => buildNumber("0")} label="0" doubleSize={true} />
+                <CalculatorButton onPress={() => buildNumber(".")} label="." />
+                <CalculatorButton onPress={calculateResult} label="=" color={colors.orange} />
             </View>
 
         </View>
